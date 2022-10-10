04a2a2b3913238faa520a437c400ba008b620bb4
Bello Ibrahim <94493245+belloibrahv@users.noreply.github.com>
2022-10-10 14:02:14 -0400

    Deploy wedlo (#24)
    * Deployed App
    
    * Deployed App
    
    * Deployed App

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-      <Router basename='wedlo'>
-        <App />
-      </Router>
+       <Router basename='wedlo'>
+         <App />
+       </Router>
     </React.StrictMode>
 );
 
